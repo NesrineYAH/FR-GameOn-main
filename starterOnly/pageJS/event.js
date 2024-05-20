5cf1b95df9d824e03dbada8b83f30edf91832067
NesrineYAH <nesrineyahoum@gmail.com>
2024-05-20 23:35:09 +0200

    //

--- starterOnly/pageJS/event.js
+++ starterOnly/pageJS/event.js
@@ -4,7 +4,7 @@ const totalSlides = slides.length; //9
 
 function showSlide(index) {
   slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${200 * (i - index)}%)`;
+    slide.style.transform = `translateX(${100 * (i - index)}%)`;
   });
 }
 
@@ -21,7 +21,7 @@ function autoNextSlide() {
   nextSlide(); // Appel de la fonction nextSlide
 }
 // Appeler autoNextSlide toutes les 3 secondes (par exemple) à reactiver la ligne 24
-//const intervalId = setInterval(autoNextSlide, 10000);
+const intervalId = setInterval(autoNextSlide, 5000);
 showSlide(currentIndex);
 
 function openSidebar() {
